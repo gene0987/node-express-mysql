e9e7868bfc2b7c9bb9be65bf35b1940526c7b038
gene09876543@gmail.com <gene0987>
2021-07-08 14:29:03 +0800

    uncommented getAll function that uses promise

--- controller/PersonController.js
+++ controller/PersonController.js
@@ -4,31 +4,31 @@ const personService = require("../service/PersonService");
 // to tidy up the code
 
 // no promise
-const getAll = (req, res) => {
-  try {
-    console.log("retrieving all data");
-    const result = personService.getAll();
-    console.log("controller layer: ", result);
-    res.status(200);
-    res.send(result);
-  } catch (err) {
-    console.log(err);
-    res.status(500);
-    res.send(err);
-  }
-};
+// const getAll = (req, res) => {
+//   try {
+//     console.log("retrieving all data");
+//     const result = personService.getAll();
+//     console.log("controller layer: ", result);
+//     res.status(200);
+//     res.send(result);
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500);
+//     res.send(err);
+//   }
+// };
 
 // with promise
-// const getAll = (req, res) => {
-//   console.log("retrieving all data");
-//   personService
-//     .getAll(req, res)
-//     .then((results) => {
-//       console.log("controller layer: ", results);
-//       res.send(results);
-//     })
-//     .catch((err) => console.log(err));
-// };
+const getAll = (req, res) => {
+  console.log("retrieving all data");
+  personService
+    .getAll(req, res)
+    .then((results) => {
+      console.log("controller layer: ", results);
+      res.send(results);
+    })
+    .catch((err) => console.log(err));
+};
 
 const getById = (req, res) => {
   personService
